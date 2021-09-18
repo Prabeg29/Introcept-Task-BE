2d14112a0344b16438fb04a16995a03cce44880e
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-09-18 18:08:54 +0545

    Refactor

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -13,9 +13,6 @@ class FormController extends Controller
 
     public function submit(SubmitFormRequest $request)
     {
-        if($request->validated()){
-            return back()->with('success', 'Thanks for form submission.');
-        }
-        return back()->withInput();
+        return redirect('form')->with('success', 'Form submitted successfully.');
     }
 }
