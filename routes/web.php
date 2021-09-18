6da27fbec7eaefad07a2835110eee3198aae9d20
Prabeg Shakya <shakyaprabeg@gmail.com>
2021-09-18 10:09:16 +0545

    Add routes to web.php

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('form', [FormController::class, 'index']);
+Route::post('form', [FormController::class, 'submit']);
+
 Route::get('/', function () {
     return view('welcome');
 });
